59467942ca6279a406558615b486f9b0ec128f0b
Yusufhan Doğan <yusufhanfurkan@hotmail.com>
2023-09-15 19:31:39 +0300

    step_four_darussalam_page_links

--- SampleData/spiders/sampledataspider.py
+++ SampleData/spiders/sampledataspider.py
@@ -18,28 +18,8 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
 
         urls = [
-                'https://www.rumah.com/agen-properti/di-yogyakarta/',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/2',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/3',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/4',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/5',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/6',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/7',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/8',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/9',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/10',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/11',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/12',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/13',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/14',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/15',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/16',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/17',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/18',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/19',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/20',
-                'https://www.rumah.com/agen-properti/di-yogyakarta/21',
-
+                "https://www.rumah.com/agen-properti/nanggroe-aceh-darussalam",
+                "https://www.rumah.com/agen-properti/nanggroe-aceh-darussalam/2"
         ]
 
         for url in urls:
@@ -53,40 +33,3 @@ class QuotesSpider(scrapy.Spider):
             }
 
 
-
-        """items = names()
-        name = response.xpath('//*[@id="wrapper-inner"]/div[8]/div/div/div/section/div[3]/div[1]/div[1]/div[3]/a/@href').extract_first()
-        items['name'] = name
-
-        yield items"""
-
-
-"""                "https://www.rumah.com/agen-properti/bali",
-                    "https://www.rumah.com/agen-properti/di-yogyakarta",
-                    "https://www.rumah.com/agen-properti/nanggroe-aceh-darussalam",
-                    "https://www.rumah.com/agen-properti/kep-bangka-belitung",
-                    "https://www.rumah.com/agen-properti/bengkulu",
-                    "https://www.rumah.com/agen-properti/banten",
-                    "https://www.rumah.com/agen-properti/gorontalo",
-                    "https://www.rumah.com/agen-properti/jambi",
-                    "https://www.rumah.com/agen-properti/jawa-barat",
-                    "https://www.rumah.com/agen-properti/jawa-timur",
-                    "https://www.rumah.com/agen-properti/jawa-tengah",
-                    "https://www.rumah.com/agen-properti/kalimantan-barat",
-                    "https://www.rumah.com/agen-properti/kalimantan-timur",
-                    "https://www.rumah.com/agen-properti/kep-riau",
-                    "https://www.rumah.com/agen-properti/kalimantan-selatan",
-                    "https://www.rumah.com/agen-properti/kalimantan-tengah",
-                    "https://www.rumah.com/agen-properti/lampung",
-                    "https://www.rumah.com/agen-properti/maluku",
-                    "https://www.rumah.com/agen-properti/maluku-utara",
-                    "https://www.rumah.com/agen-properti/nusa-tenggara-barat",
-                    "https://www.rumah.com/agen-properti/nusa-tenggara-timur", 
-                    "https://www.rumah.com/agen-properti/papua",
-                    "https://www.rumah.com/agen-properti/papua-barat",
-                    "https://www.rumah.com/agen-properti/riau",
-                    "https://www.rumah.com/agen-properti/sulawesi-utara",
-                    "https://www.rumah.com/agen-properti/sumatera-barat",
-                    "https://www.rumah.com/agen-properti/sumatera-selatan",
-                    "https://www.rumah.com/agen-properti/sulawesi-tengah",
-                    "https://www.rumah.com/agen-properti/sumatera-utara"""
