f3bd63ce39eaf72683ba15cdd5e119c9adcb344e
Yusufhan Doğan <yusufhanfurkan@hotmail.com>
2023-09-15 20:04:38 +0300

    step_eight_timur_page_links

--- SampleData/spiders/sampledataspider.py
+++ SampleData/spiders/sampledataspider.py
@@ -18,10 +18,32 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
 
         urls = [
-            'https://www.rumah.com/agen-properti/jambi',
-            'https://www.rumah.com/agen-properti/jambi/2',
-            'https://www.rumah.com/agen-properti/jambi/3',
-            'https://www.rumah.com/agen-properti/jambi/4'
+            'https://www.rumah.com/agen-properti/jawa-timur/2',
+            'https://www.rumah.com/agen-properti/jawa-timur/3',
+            'https://www.rumah.com/agen-properti/jawa-timur/4',
+            'https://www.rumah.com/agen-properti/jawa-timur/5',
+            'https://www.rumah.com/agen-properti/jawa-timur/6',
+            'https://www.rumah.com/agen-properti/jawa-timur/7',
+            'https://www.rumah.com/agen-properti/jawa-timur/8',
+            'https://www.rumah.com/agen-properti/jawa-timur/9',
+            'https://www.rumah.com/agen-properti/jawa-timur/10',
+            'https://www.rumah.com/agen-properti/jawa-timur/11',
+            'https://www.rumah.com/agen-properti/jawa-timur/12',
+            'https://www.rumah.com/agen-properti/jawa-timur/13',
+            'https://www.rumah.com/agen-properti/jawa-timur/14',
+            'https://www.rumah.com/agen-properti/jawa-timur/15',
+            'https://www.rumah.com/agen-properti/jawa-timur/16',
+            'https://www.rumah.com/agen-properti/jawa-timur/17',
+            'https://www.rumah.com/agen-properti/jawa-timur/18',
+            'https://www.rumah.com/agen-properti/jawa-timur/19',
+            'https://www.rumah.com/agen-properti/jawa-timur/20',
+            'https://www.rumah.com/agen-properti/jawa-timur/21',
+            'https://www.rumah.com/agen-properti/jawa-timur/22',
+            'https://www.rumah.com/agen-properti/jawa-timur/23',
+            'https://www.rumah.com/agen-properti/jawa-timur/24',
+            'https://www.rumah.com/agen-properti/jawa-timur/25',
+            'https://www.rumah.com/agen-properti/jawa-timur/26',
+            'https://www.rumah.com/agen-properti/jawa-timur/27',
 
         ]
 
